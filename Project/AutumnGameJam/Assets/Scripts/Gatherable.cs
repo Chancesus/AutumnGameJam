364e506e64e75e8556eba9d0964f59de4005b0e5
TheUnaverageJoe <jcarter94043@gmail.com>
2022-09-13 15:56:21 -0700

    Colleting objects started

--- Project/AutumnGameJam/Assets/Scripts/Gatherable.cs
+++ Project/AutumnGameJam/Assets/Scripts/Gatherable.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Gatherable : MonoBehaviour
 {
+    [System.Serializable]
+    public class LocationData{
+        [SerializeField]private Vector3 _position;
+        [SerializeField]private Vector3 _rotation;
+        public LocationData(Vector3 pos, Vector3 rot){
+            _position = pos;
+            _rotation = rot;
+        }
+        public LocationData(){
+            _position = Vector3.zero;
+            _rotation = Vector3.zero;
+        }
+    }
 
-    [SerializeField] private List<Transform> _spawnLocations;
+    [SerializeField] private List<LocationData> _spawnLocations;
     public static System.Action<Gatherable> onFound;
-    public Transform PickRandomSpawn(){
+
+    void Start(){
+        onFound += OnFoundCallback;
+    }
+    public LocationData PickRandomSpawn(){
         if(_spawnLocations == null || _spawnLocations.Count == 0){
             Debug.LogError("List is empty");
             return null;
@@ -15,5 +32,16 @@ public class Gatherable : MonoBehaviour
         
         int index = Mathf.FloorToInt(Random.Range(0, _spawnLocations.Count));
         return _spawnLocations[index];
+    }
+
+    public void PlayerFoundObject(){
+        onFound?.Invoke(this);
+    }
+
+    private void OnFoundCallback(Gatherable _self){
+        Destroy(_self.gameObject);
+    }
+    void OnDestroy(){
+        onFound -= OnFoundCallback;
     }
 }
