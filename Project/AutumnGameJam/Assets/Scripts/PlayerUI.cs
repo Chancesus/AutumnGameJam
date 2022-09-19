be5ad9179de784a1b1c74195f1f2ceb5e51415be
Chancesus <chancekgsmith@gmail.com>
2022-09-19 14:54:25 -0700

    PlayerUI Script and Item Menu
    Designed Player Item Menu and changed PlayerUI script so that the player can pull up with tab.

--- Project/AutumnGameJam/Assets/Scripts/PlayerUI.cs
+++ Project/AutumnGameJam/Assets/Scripts/PlayerUI.cs
@@ -8,12 +8,28 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private TMP_Text _foundTextField;
 
     [SerializeField] private Gatherables _gameManagerGatherables;
+    [SerializeField] private CanvasGroup _itemPanel;
 
     void Start()
     {
         SetStartText();
         _gameManagerGatherables.onRemainingUpdated += UpdateFoundText;
+        _itemPanel.alpha = 0;
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            _itemPanel.alpha = 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            _itemPanel.alpha = 0;
+        }
+    }
+
     private void SetStartText(){
        _foundTextField.text = "Remaining: " + _gameManagerGatherables.GetRemainingGatherables();
     }
